a7cf472da9d92505725febb8c4ddaeede5f0eb1b
Edukons <eduardo.kons17@gmail.com>
2024-08-23 16:47:23 -0300

    IntegerChoices na model Pessoa.

--- core/models/pessoa.py
+++ core/models/pessoa.py
@@ -7,10 +7,15 @@ class Pessoa(models.Model):
     data_nasc = models.DateField
     foto = models.ImageField
     class escolaridade(models.Model):
-        class StatusEscolaridade(models.IntergerChoices):
+        class StatusEscolaridade(models.IntegerChoices):
             NENHUMA = 1, "Nenhuma"
             FUNDAMENTAL_INCOMPLETO = 2, "Fundamental Incompleto"
             FUNDAMENTAL_COMPLETO = 3, "Fundamental Completo"
             ENSINO_MEDIO_INCOMPLETO = 4, "Ensino Médio Incompleto"
             ENSINO_MEDIO_COMPLETO = 5, "Ensino Médio Completo"
-            SUPERIOR_COMPLETO = 6
+            SUPERIOR_INCOMPLETO = 6, "Ensino Superior Incompleto"
+            SUPERIOR_COMPLETO = 7, "Ensino Superior Completo"
+            OUTRO = 8, "Outro"
+        status_escolaridade = models.IntegerField(choices=StatusEscolaridade.choices, default=1)
+
+
