ddb5bb4e85101fdb732acfe7182dbdaf167ad15b
Edukons <eduardo.kons17@gmail.com>
2024-09-06 16:46:10 -0300

    Finalização dos relacionamentos das models familía/casa , familia/pessoas e animal/prontuário

--- core/models/familia.py
+++ core/models/familia.py
@@ -3,7 +3,7 @@ from .pessoa import Pessoa
 from .casa import Casa
 
 class Familia(models.Model):
-    pessoas = models.ForeignKey(Pessoa, on_delete=models.PROTECT, related_name="pessoas", null=True, blank=True)
+    pessoas = models.ManyToManyField(Pessoa, related_name="pessoas", blank=True)
     casa = models.ForeignKey(Casa, on_delete=models.PROTECT, related_name="casa", null=True, blank=True)
     def __str__(self):
-        return self.pessoas
+        return f"Casa {self.casa.numero_casa}"
--- core/models/prontuario.py
+++ core/models/prontuario.py
@@ -4,7 +4,6 @@ from .animal import Animal
 class Prontuario(models.Model):
     data_adoeceu = models.DateField(blank=True, null=True)
     descricao = models.CharField(max_length=500)
-    nome = models.ForeignKey(Animal, related_name="prontuarios", on_delete= models.PROTECT, null=True, blank=True)
+    animal = models.ForeignKey(Animal, related_name="prontuarios", on_delete= models.PROTECT, null=True, blank=True)
     def __str__(self):
-        return self.nome
-    
+        return self.animal.nome
