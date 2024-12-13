5dbcfbcf3f76d3faa4d8d984ad57f13a2f7e7539
Edukons <eduardo.kons17@gmail.com>
2024-12-13 16:52:51 -0300

    Alteração na imagem do animal

--- core/serializers/animal.py
+++ core/serializers/animal.py
@@ -1,8 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from core.models import Animal
 
 class AnimalSerializer(ModelSerializer):
+    fotoAnimal_attachment_key = SlugRelatedField(
+        source="fotoAnimal",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    fotoAnimal = ImageSerializer(
+        required=False,
+        read_only=True
+    )
     class Meta:
         model = Animal
         fields = "__all__"
