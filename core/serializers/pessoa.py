fbe4d69f723185e9c903505269e8aba13f79d912
Edukons <eduardo.kons17@gmail.com>
2024-12-17 09:39:14 -0300

    Ajustes na página de pessoas

--- core/serializers/pessoa.py
+++ core/serializers/pessoa.py
@@ -1,8 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from core.models import Pessoa
 
 class PessoaSerializer(ModelSerializer):
+    fotoPessoa_attachment_key = SlugRelatedField(
+        source="fotoAnimal",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    fotoPessoa = ImageSerializer(
+        required=False,
+        read_only=True
+    )
     class Meta:
         model = Pessoa
         fields = ("__all__")
